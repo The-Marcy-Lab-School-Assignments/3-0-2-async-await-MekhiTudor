f5b6ad2711f9d570e02ed9eb27b26edd001e237e
MekhiTudor <164079314+MekhiTudor@users.noreply.github.com>
2024-06-19 16:52:36 -0400

    done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,2 +1,21 @@
-export const fetchHandler = () => {
+export const fetchHandler = async (url, options = {}) => {
+  try {
+    const fetchCall = await fetch(url, options);
+    if (!fetchCall.ok) {
+      throw new Error(
+        `Fetch failed with status - ${res.status}, ${res.statusText}`
+      );
+    }
+    const isJson = (fetchCall.headers.get("content-type") || "").includes(
+      "application/json"
+    );
+    if (isJson) {
+      return [await fetchCall.json(), null];
+    } else {
+      return [await fetchCall.text(), null];
+    }
+  } catch (error) {
+    console.warn(error);
+    return [null, error];
+  }
 };
